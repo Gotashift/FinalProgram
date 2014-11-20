63f160685597b15ccd2f549e074eb289a4146be8
Gotashift <chase.krause@live.bemidjistate.edu>
2014-11-20 09:13:54 -0600

    Create CDAccs.cpp

---
+++ CDAccs.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <fstream>
+
+
+
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::getline;
+
+int CDTotal = 0;
+int newesttotal = 0;
+
+// if Cash deposit, account, it will take the current variable initially set at 0 and add the amount of the checking account to it
+while (readfile.good())
+	{
+		newesttotal = amount;
+		CDTotal = CDTotal + newesttotal;
+	}
